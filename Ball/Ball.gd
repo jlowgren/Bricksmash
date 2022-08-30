3d8efa949843e3c1156e908fa17e26f1394e6875
Joacim de la Motte <joacim.delamotte@gmail.com>
2022-08-30 19:24:54 +0200

    feat: create new ball when previous one is destroyed

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -1,11 +1,21 @@
 extends KinematicBody2D
 
+signal ball_destroyed
+
 export var speed = 4
 
-var velocity = Vector2(rand_range(-10, 10), -25)
+var velocity = Vector2.ZERO
+
+func _ready():
+	randomize()
+	velocity = Vector2(rand_range(-10, 10), -25)
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * speed * delta)
 	if collision:
 		$BallBounceSound.play()
 		velocity = velocity.bounce(collision.normal)
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+	emit_signal("ball_destroyed")
--- World/World.gd
+++ World/World.gd
@@ -6,3 +6,15 @@ func _input(event):
 
 	if event.is_action_pressed("toggle_fullscreen"):
 		OS.set_window_fullscreen(!OS.window_fullscreen)
+
+func _ready():
+	create_ball()
+
+func _on_ball_destroyed():
+	create_ball()
+
+func create_ball():
+	var ball_scene = load("res://Ball/Ball.tscn")
+	var ball = ball_scene.instance()
+	ball.connect("ball_destroyed", self, "_on_ball_destroyed")
+	call_deferred("add_child", ball)
